33a162a035e1b73cd45a8a88768ba01393ba1e05
Seb <sebastiancantergiani@gmail.com>
2025-04-14 23:12:25 -0400

    se agrega pyright para hover de funciones

--- lua/potato/plugins/lsp.lua
+++ lua/potato/plugins/lsp.lua
@@ -212,7 +212,20 @@ return {
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {
+          settings = {
+            pyright = {
+              -- Using Ruff's import organizer
+              disableOrganizeImports = true,
+            },
+            python = {
+              analysis = {
+                -- Ignore all files for analysis to exclusively use Ruff for linting
+                ignore = { '*' },
+              },
+            },
+          },
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
